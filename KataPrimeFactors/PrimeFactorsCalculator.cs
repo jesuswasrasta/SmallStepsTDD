5fba4f956000ff9ccedab68bfe9a00fd550072c2
Ferdinando Santacroce <jesus_was_rasta@yahoo.it>
2016-11-06 15:44:35 +0100

    Step 2: create class and method Calculate() that returns null

---
+++ KataPrimeFactors/PrimeFactorsCalculator.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+
+namespace KataPrimeFactors
+{
+	public class PrimeFactorsCalculator
+	{
+		public List<int> Calculate(int number)
+		{
+			return null;
+		}
+	}
+}
