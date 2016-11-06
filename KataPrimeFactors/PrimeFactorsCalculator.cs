c106321e4eea74ab3b7108842244ccf1d17cd505
Ferdinando Santacroce <jesus_was_rasta@yahoo.it>
2016-11-06 16:59:59 +0100

    Step 9: need to add an if (n % 2 == 0) to pass it, but test for 2 will now fail

--- KataPrimeFactors/PrimeFactorsCalculator.cs
+++ KataPrimeFactors/PrimeFactorsCalculator.cs
@@ -10,8 +10,14 @@ namespace KataPrimeFactors
 			var factors = new List<int>();
 			if (n > 1)
 			{
+				if (n % 2 == 0)
+				{
+					factors.Add(2);
+					n /= 2;
+				}
 				factors.Add(n);
 			}
+
 			return factors;
 		}
 	}
