9d54106050f89506f8c8ec38e59014d52b21c5e9
Ferdinando Santacroce <jesus_was_rasta@yahoo.it>
2016-11-06 12:29:31 +0100

    Empty solution

---
+++ KataPrimeFactors.Tests/PrimeFactorsCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+
+namespace KataPrimeFactors.Tests
+{
+	public class PrimeFactorsCalculatorTests
+    {
+		[Test]
+		public void Nothing()
+		{
+			//Ok, just testing everything is ready before the kata start! :)
+		}
+	}
+}
