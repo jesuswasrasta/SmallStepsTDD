43da676f92e64a0b86ca7af52a2f8c987812bff2
Ferdinando Santacroce <jesus_was_rasta@yahoo.it>
2016-11-06 21:56:51 +0100

    Step 11: move the last if (n > 1) outside the first

--- KataPrimeFactors/PrimeFactorsCalculator.cs
+++ KataPrimeFactors/PrimeFactorsCalculator.cs
@@ -15,10 +15,10 @@ namespace KataPrimeFactors
 					factors.Add(2);
 					n /= 2;
 				}
-				if (n > 1)
-				{
-					factors.Add(n);
-				}
+			}
+			if (n > 1)
+			{
+				factors.Add(n);
 			}
 			return factors;
 		}
