64abf28af1785779cddacd23843a92dacae24097
ferdinando.santacroce <santacroce@intre.it>
2016-11-22 15:31:41 +0100

    Add empty solution for Prime Factors Kata

---
+++ KataPrimeFactors/KataPrimeFactors/PrimeFactorsCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace KataPrimeFactors
+{
+	public class PrimeFactorsCalculator
+	{
+		public IEnumerable Calculate(int n)
+		{
+			var factors = new List<int>();
+			for (var divisor = 2; n > 1; divisor++)
+				for (; n%divisor == 0; n /= divisor)
+					factors.Add(divisor);
+			return factors;
+		}
+	}
+}
